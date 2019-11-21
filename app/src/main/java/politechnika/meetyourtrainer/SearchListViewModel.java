fbff58baa6e4fe50287782ccd4f9dd634fa153d9
Hubert Liberadzki <209377@edu.p.lodz.pl>
2019-11-21 09:42:01 +0100

    search view

---
+++ app/src/main/java/politechnika/meetyourtrainer/SearchListViewModel.java
@@ -0,0 +1,42 @@
+package politechnika.meetyourtrainer;
+
+import androidx.lifecycle.ViewModel;
+
+public class SearchListViewModel extends ViewModel {
+    private String name;
+    private String surname;
+    private String age;
+    private String value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
