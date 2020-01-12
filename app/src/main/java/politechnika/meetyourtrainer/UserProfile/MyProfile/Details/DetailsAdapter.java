bd0990c6dead31730dc0d9f55344d6405785af81
liberadzkih <liberadzkih@gmail.com>
2020-01-12 02:04:28 +0100

    Deleted doubled my Ads

--- app/src/main/java/politechnika/meetyourtrainer/UserProfile/MyProfile/Details/DetailsAdapter.java
+++ app/src/main/java/politechnika/meetyourtrainer/UserProfile/MyProfile/Details/DetailsAdapter.java
@@ -14,21 +14,18 @@ public class DetailsAdapter extends FragmentStatePagerAdapter {
     public Fragment getItem(int position) {
         if (position == 0)
             return AboutMe.newInstance();
-        else if (position == 1)
-            return Comments.newInstance();
         else
-            return Ads.newInstance();
+            return Comments.newInstance();
     }
 
     @Override
     public int getCount() {
-        return 3;
+        return 2;
     }
 
     @Override
     public String getPageTitle(int position) {
         if (position == 0) return "About me";
-        else if (position == 1) return "Comments";
-        else return "Ads";
+        else return "Comments";
     }
 }
