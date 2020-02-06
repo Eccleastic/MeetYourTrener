054c1e611218d88090c90987403a44ba79552b7a
liberadzkih <liberadzkih@gmail.com>
2020-02-06 01:39:44 +0100

    removed bugs that destroyed the application when a new user added an ad

--- app/src/main/java/politechnika/meetyourtrainer/Search/SearchListView.java
+++ app/src/main/java/politechnika/meetyourtrainer/Search/SearchListView.java
@@ -69,19 +69,25 @@ public class SearchListView extends Fragment {
                     m.setImgURL("https://thumbs.dreamstime.com/z/ludzie-biznesu-oceny-quesiton-wzruszenie-ramionami-279604.jpg");
                     models.add(m);
                 } else {
-                    for(int i=0; i<result.length(); i++) {
+                    for (int i = 0; i < result.length(); i++) {
                         CardModel m = new CardModel();
                         JSONObject obj = result.getJSONObject(i);
                         m.setDescription(obj.getString("ad_description"));
                         m.setTitle(obj.getString("title"));
-                        m.setImgURL(obj.getString("photo_link"));
+                        try {
+                            m.setImgURL(obj.getString("photo_link"));
+                            m.setEmail(obj.getString("trener_email"));
+                            m.setPhone(obj.getString("trener_phone"));
+                        } catch (JSONException e) {
+                            m.setImgURL("https://www.mixmedia.pl/temp/aktualnosci/znak%20zapytania.jpg");
+                            m.setEmail("random@email.com");
+                            m.setPhone("123456789");
+                        }
                         m.setAd_id(obj.getString("advertisement_id"));
                         m.setAddress(obj.getString("address"));
                         m.setName(obj.getString("trener_name"));
                         m.setPrice(String.valueOf(obj.getDouble("price")));
                         m.setTrener_id(String.valueOf(obj.getInt("trener_id")));
-                        m.setEmail(obj.getString("trener_email"));
-                        m.setPhone(obj.getString("trener_phone"));
                         m.setDate(obj.getString("date") + " " + obj.getString("time"));
                         models.add(m);
                     }
